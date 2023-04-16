88072d190551a7a5817f3e4a3978d0011dd51723
saranreddy141 <88263750+saranreddy141@users.noreply.github.com>
2023-04-17 01:45:58 +0530

    Update server.js

--- nodeBudget/server.js
+++ nodeBudget/server.js
@@ -16,32 +16,64 @@ db.serialize(() => {
   );
 });
 
-app.post("/api/register", async (req, res) => {
+app.post("/api/register", (req, res) => {
   const { username, email, password } = req.body;
+  let dbData = `select * from users where email = ${email} or username = ${username}`;
 
-  const sql = "INSERT INTO users (username,email, password) VALUES (?, ?, ?)";
-  db.run(sql, [username, email, password], (err) => {
+  db.run(dbData, (err, data) => {
+    dbData = "";
     if (err) {
       console.log(err);
-      res.status(500).json({ message: "Error registering user" });
     } else {
-      res.status(200).json({ message: "User registered successfully" });
+      dbData = data;
     }
   });
+
+  const emailRegex =
+    /^[a-zA-Z0-9_]([\.-]?[a-zA-Z0-9_])*@[a-zA-Z0-9_]([\.-]?[a-zA-Z0-9_])*(\.[a-zA-Z0-9_]{2,})+$/;
+
+  if (email && email.match(emailRegex) && username && password && !dbData) {
+    const sql = "INSERT INTO users (username,email, password) VALUES (?, ?, ?)";
+    db.run(sql, [username, email, password], (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).json({ message: "Error registering user" });
+      } else {
+        res
+          .status(200)
+          .json({ message: "Registered successfully", status: "ok" });
+      }
+    });
+  } else if (!username || dbData) {
+    res.status(400).json({
+      message: "Please enter valid username or username already exist",
+    });
+  } else if (!password) {
+    res.status(400).json({
+      message: "Please provide password",
+    });
+  } else {
+    res.status(400).json({
+      message: "Please enter valid email",
+    });
+  }
 });
 
 app.post("/api/login", async (req, res) => {
   const { username, password } = req.body;
 
-  const sql = "SELECT * FROM users where email = ? and password=?";
-  db.get(sql, [username, password], async (err, user) => {
+  const sql =
+    "SELECT * FROM users where (email = ? or username = ? )and password=?";
+  db.get(sql, [username, username, password], async (err, user) => {
     if (err) {
       console.log(err);
       res.status(500).json({ message: "Error logging in" });
     } else if (!user) {
       res.status(401).json({ message: "Invalid email or password" });
     } else {
-      res.status(200).json({ user });
+      res
+        .status(200)
+        .json({ user, message: "Login Successfully", status: "ok" });
     }
   });
 });
